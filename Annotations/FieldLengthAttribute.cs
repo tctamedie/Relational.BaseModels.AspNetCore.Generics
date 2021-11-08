89932bcd15e5f3a41031c64215704c4aa109409b
Tamandani Chagwirampeni <tctamedie@gmail.com>
2021-11-08 07:25:05 +0200

    Added Required and FieldLength Annotation for UI Validation

---
+++ Annotations/FieldLengthAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Relational.BaseModels.AspNetCore.Generics.Annotations
+{
+    public class FieldLengthAttribute : StringLengthAttribute
+    {
+        public FieldLengthAttribute(int maximumLength, [CallerMemberName] string id = "") : base(maximumLength)
+        {
+
+            ID = id.FirstLetterToLower();
+        }
+        public string ID { get; }
+    }
+}
---
+++ Annotations/RequiredFieldModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Relational.BaseModels.AspNetCore.Generics.Annotations
+{
+    public class RequiredFieldModel 
+    {
+        public RequiredFieldModel(string id = "", string ErrorMessage = "", bool allowEmptyStrings=false) 
+        {
+            AllowEmptyStrings = allowEmptyStrings;
+            ID = id.FirstLetterToLower();
+            Message = ErrorMessage;
+        }
+        public string ID { get; set; }
+        public string Message { get; }
+        public bool AllowEmptyStrings { get; }
+    }
+}
